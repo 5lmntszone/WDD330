97d7d92524033f04aa5a30c220f402e6aa4b232e
Barbara <5elementszone@gmail.com>
2025-07-26 19:38:03 +0200

    fixing styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import Alert from './Alert';
+import Alert from './Alert.mjs';
 
 const dataSource = new ProductData('tents');
 const listElement = document.querySelector('.product-list');
 const productList = new ProductList('tents', dataSource, listElement);
-const alerts = new Alert('json/alerts.json');
+const alerts = new Alert('./json/alerts.json');
 
 alerts.init();
 productList.init();
