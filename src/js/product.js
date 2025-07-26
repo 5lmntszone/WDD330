08ab9d8ebe09b8923e9ee6f425de56080be7a757
Barbara <5elementszone@gmail.com>
2025-07-26 15:54:12 +0200

    added dynamic prod det pages

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,13 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+// Get product id from URL
+const productId = getParam("product");
+
+// Create data source instance
 const dataSource = new ProductData("tents");
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Create product details instance and initialize
+const product = new ProductDetails(productId, dataSource);
+product.init();
