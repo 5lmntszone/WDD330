64082a1c0695b3a1b4967b662880ae7cef29fc20
Barbara <5elementszone@gmail.com>
2025-08-02 16:26:29 +0200

    added ShoppingCart.mjs

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,33 @@
+import { getLocalStorage } from "./utils.mjs";
+
+export default class ShoppingCart {
+  constructor(key, listElement) {
+    this.key = key;
+    this.listElement = listElement;
+  }
+
+  init() {
+    const cartItems = getLocalStorage(this.key) || [];
+    this.renderCart(cartItems);
+  }
+
+  renderCart(cartItems) {
+    const template = document.getElementById("cart-item-template");
+    this.listElement.innerHTML = ""; 
+
+    cartItems.forEach((item) => {
+      const clone = template.content.cloneNode(true);
+
+      clone.querySelector(".cart-card__image").href = `../product_pages/?product=${item.Id}`;
+      clone.querySelector("img").src = `../${item.Image}`;
+      clone.querySelector("img").alt = item.Name;
+      clone.querySelector("a + a").href = `../product_pages/?product=${item.Id}`;
+      clone.querySelector(".card__name").textContent = item.Name;
+      clone.querySelector(".cart-card__color").textContent = item.Colors[0].ColorName;
+      clone.querySelector(".cart-card__quantity").textContent = `qty: 1`;
+      clone.querySelector(".cart-card__price").textContent = `$${item.FinalPrice}`;
+
+      this.listElement.appendChild(clone);
+    });
+  }
+}
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,10 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
-
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
-
-function cartItemTemplate(item) {
-  const imagePath = `../${item.Image}`;
-
-  return `<li class="cart-card divider">
-    <a href="../product_pages/?product=${item.Id}" class="cart-card__image">
-      <img src="${imagePath}" alt="${item.Name}" />
-    </a>
-    <a href="../product_pages/?product=${item.Id}">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-}
+import { loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter();
-  renderCartContents();
+
+  const listElement = document.querySelector(".product-list");
+  const cart = new ShoppingCart("so-cart", listElement);
+  cart.init();
 });
