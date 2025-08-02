33b0a7cdbf734a3cdaa03a68aa6307badbcad63e
Barbara <5elementszone@gmail.com>
2025-08-02 19:25:58 +0200

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  base: "/WDD330/", 
 
   build: {
     outDir: "../dist",
@@ -12,8 +13,7 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/cart/checkout.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
-        productListing: resolve(__dirname, 'product_listing/index.html')
-  
+        productListing: resolve(__dirname, "src/product_listing/index.html"),
       },
     },
   },
