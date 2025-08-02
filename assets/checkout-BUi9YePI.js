861ef7605b73d4933fe92c0b5fee73e8cf2455e5
Barbara <5elementszone@gmail.com>
2025-08-02 20:37:31 +0200

    Fix alerts, logo paths, cart link

--- assets/checkout-BUi9YePI.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./utils-W0WFFMyk.js";document.addEventListener("DOMContentLoaded",()=>{e()});
---
+++ assets/checkout-ClWW3kYZ.js
@@ -0,0 +1,1 @@
+import{l as e}from"./utils-D6Usc3I_.js";document.addEventListener("DOMContentLoaded",()=>{e()});
